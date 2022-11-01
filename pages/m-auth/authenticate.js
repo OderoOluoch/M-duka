a1c3b174cfb2ef1b55c9ea2687786ab905c2bf10
ogluoch <ogoluoch@safaricom.co.ke>
2022-11-01 16:51:57 +0300

    Added function name

--- pages/m-auth/authenticate.js
+++ pages/m-auth/authenticate.js
@@ -2,9 +2,12 @@ import redirect from 'nextjs-redirect'
 
 const Redirect = redirect('https://m-auth-2.vercel.app/auth/authenticator')
 
-export default () => (
-  <Redirect>
+
+export default function Authenticate() {
+  return (
+    <Redirect>
     {/* <MyLayout>Redirecting </MyLayout> */}
     <h1>Redirecting to Mpesa</h1>
   </Redirect>
-)
+  );
+}
--- pages/m-auth/checkout.js
+++ pages/m-auth/checkout.js
@@ -2,9 +2,12 @@ import redirect from 'nextjs-redirect'
 
 const Redirect = redirect('https://m-auth-2.vercel.app/auth/checkout')
 
-export default () => (
-  <Redirect>
+
+export default function Checkout() {
+  return (
+    <Redirect>
     {/* <MyLayout>Redirecting </MyLayout> */}
     <h1>Redirecting to Mpesa</h1>
   </Redirect>
-)
+  );
+}
